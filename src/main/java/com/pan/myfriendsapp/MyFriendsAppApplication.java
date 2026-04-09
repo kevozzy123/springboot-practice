caadc923b3bf9d50d1541f15d0b44c2b9b86a895
Yimeng Wang <wangyimeng1216@gmail.com>
2026-04-08 21:34:26 -0400

    first commit

---
+++ src/main/java/com/pan/myfriendsapp/MyFriendsAppApplication.java
@@ -0,0 +1,13 @@
+package com.pan.myfriendsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFriendsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyFriendsAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pan/myfriendsapp/MyFriendsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pan.myfriendsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFriendsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
